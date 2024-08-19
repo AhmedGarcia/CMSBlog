d46fc8366f5b714333a8b8fab44860ab2c8b91b8
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-08-19 16:14:30 -0400

    populating the homeRoutes. added a route to display all posts in the homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
+
+
+// Route for the homepage that displays all posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true}));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route to display a single post by ID
+router.get('/post/:id', async (req, res) => {})
