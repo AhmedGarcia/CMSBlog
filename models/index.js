c36b071d2de1080580ae5c66a5cac5bb1d3caa58
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-08-19 14:53:38 -0400

    populated models/index.js to establish assosiationsbetween models

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Assosiations between the models
+
+User.ahasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignkey: 'post_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment };
