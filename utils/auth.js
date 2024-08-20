5b58c58b045eee3a22edbee0493ce7d0310c4957
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-08-20 13:49:33 -0400

    added the withAuth middleware to check if logged in before accessing routes

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Middleware to check if the user if logged in before accessing certain routes
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
