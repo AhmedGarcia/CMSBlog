db819389e5736ba050aeafb67bedd1973504d1b5
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-08-21 10:55:00 -0400

    populated the main handlebar. tried to get the app running. had to install pg to the dependencies. fixed a typo  in line 1 of  models/comment.js

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const seedUsers = require('./userData');
+const seedPosts = require('./postData');
+const seedComments = require('../commentData');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n--- Database Synced ---\n');
+
+    await seedUsers();
+    console.log('\n---Users Seeded ---\n');
+
+    await seedPosts();
+  console.log('\n--- Posts Seeded ---\n');
+
+  await seedComments();
+  console.log('\n--- Comments Seeded ---\n');
+
+  process.exit(0);
+};
+
+seedAll();
+
