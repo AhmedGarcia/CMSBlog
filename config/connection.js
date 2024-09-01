82712b400081b6ae8f8f32e3b94581cd028289b2
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-01 15:50:16 -0400

    removed ssl setting in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,9 @@ require('dotenv').config();
 console.log("Database URL:", process.env.DATABASE_URL); 
 const Sequelize = require('sequelize');
 
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // For self-signed certificates
-        }
-    },
 });
 
 module.exports = sequelize;
