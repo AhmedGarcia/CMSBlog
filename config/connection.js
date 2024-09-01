e4797fb69ad2d532d5cf353ad0ab423bd1315eda
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-01 15:57:55 -0400

    modified connection.js to be more explisit. removed the DATABASE_URL variable

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
-console.log("Database URL:", process.env.DATABASE_URL); 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME, // Database name
+    process.env.DB_USER, // Username
+    process.env.DB_PASSWORD, // Password
+    {
+        host: process.env.DB_HOST, // Hostname
+        dialect: 'postgres', // Dialect (PostgreSQL in this case)
+        port: process.env.DB_PORT || 5432, // Port number (default to 5432)
+    }
+);
 
 module.exports = sequelize;
 
