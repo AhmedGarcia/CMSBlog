f897c4d49fcbf69d72d747a78b1fa21e525a4b56
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-01 15:08:22 -0400

    added DATABASE_URL to .env file, made changes to connect.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Establishes a connection to the database using environment variables
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || '127.0.0.1', // Fallback to '127.0.0.1' if DB_HOST is not set
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    port: process.env.DB_PORT || 5432, // Fallback to 5432 if DB_PORT is not set
-    logging: false, // Optionally disable logging for a cleaner output
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // For self-signed certificates
+        }
+    },
 });
 
 module.exports = sequelize;
+
